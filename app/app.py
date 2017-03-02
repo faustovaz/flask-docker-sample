fcc02d7993b2c3a40e71e8f1c117b62dc1cfa424
faustovaz <faustovaz@gmail.com>
2017-03-02 00:24:00 -0300

    Adding environment var to debug flask app

--- app/app.py
+++ app/app.py
@@ -5,11 +5,14 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    return "Hello World, Docker!!";
+    return "Hello World, Docker!";
 
 @app.route("/me")
 def me():
     return "Flask-docker-app";
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    if os.getenv("DEBUG") and os.getenv("DEBUG") == "1":
+        app.run(host='0.0.0.0', debug=True)
+    else:
+        app.run(host='0.0.0.0')
